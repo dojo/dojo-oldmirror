2e1e84ecd7829b9f17311ffb621fbfba27653aab
Colin Snover <github.com@zetafleet.com>
2012-02-16 21:45:28 +0000

    Release 1.7.2.

--- _base/kernel.js
+++ _base/kernel.js
@@ -93,9 +93,9 @@ define(["../has", "./config", "require", "module"], function(has, config, requir
 			this.revision = 0;
 		}
 	=====*/
-	var rev = "$Rev: 27849 $".match(/\d+/);
+	var rev = "$Rev: 27913 $".match(/\d+/);
 	dojo.version = {
-		major: 1, minor: 7, patch: 2, flag: "rc1",
+		major: 1, minor: 7, patch: 2, flag: "",
 		revision: rev ? +rev[0] : NaN,
 		toString: function(){
 			var v = dojo.version;
